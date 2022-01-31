e348c7ddfc7a1e2b8c2343b66f1c7e44a94b0a17
mi-wada <mtak.wd@gmail.com>
2022-01-31 22:17:32 +0900

    add dynamo

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,27 @@ use std::{
     hash::{Hash, Hasher},
 };
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+use aws_sdk_dynamodb::Client;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let url = {
         let args: Vec<String> = env::args().collect();
         args[1].clone()
     };
-    let body = reqwest::blocking::get(&url)?.text()?;
+    let body = reqwest::get(&url).await?.text().await?;
 
     println!("url: {}", url);
     let mut hasher = DefaultHasher::new();
 
     body.hash(&mut hasher);
     println!("body's hash: {}", hasher.finish());
+
+    let shared_config = aws_config::load_from_env().await;
+    let client = Client::new(&shared_config);
+    let req = client.list_tables().limit(10);
+    let resp = req.send().await?;
+    println!("tables: {:?}", resp.table_names);
 
     Ok(())
 }
